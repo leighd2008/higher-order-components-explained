def6fcff316ad2c1dcadafa13d792ee4ea00d104
leighd2008 <leighd2008@gmail.com>
2019-10-14 07:21:08 -0400

    added user-list component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import UserProfile from "./components/user-profile/user-profile.jsx";
+import UserList from "./components/user-list/user-list";
 
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
+      <UserList />
       <UserProfile name="Diane" email="leighd2008@gmail.com" />
     </div>
   );
---
+++ src/components/user-list/user-list.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class UserList extends React.Component {
+  render() {
+    return (
+      <div className="container user-list">
+        <h1> Users List </h1>
+      </div>
+    );
+  }
+}
+
+export default UserList;
